e12d314d6b80f1608eb5120cc404a34d9e836b22
Aisha-Farah <aisha.farah@student.moringaschool.com>
2021-09-02 22:14:04 +0300

    #akan to #afrmaAkan

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
-const findAkan= (e) => {
+const findAkan= function (e) {
     e.preventDefault();
     const fName =["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
     const mName =["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
     
-    const fr = document.querySelector("afrmaAkan");
+    const frm = document.querySelector("#afrmaAkan");
+
     const fd = new FormData(frm);
     const gender = fd.get("gender");
     const dob =fd.get("dob");
-
-    console.log (fName [0])
-
-
+const d =new Date(dob).getDay();
+const successAlert =document.querySelector("#success-alert");
+successAlert.classList.remove("d-none");
+successAlert.classList.add("d-block");
+if (gender == "female"){
+successAlert.innerHTML += "Your Akan name is" +fName[d]
 }
+else{
+    successAlert.innerHTML += "Your Akan name is" +mName[d]
+}
+}
+document.querySelector("#afrmaAkan").addEventListener('submit' , findAkan)
 
-document.querySelector("#frmAkan").addEventListener('submit' , checkAkan);
+ 
